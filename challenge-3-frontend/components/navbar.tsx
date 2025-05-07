da7a4d54bbcdfa2c1a1ab14b04449607a03a8228
Daniel235-web <samueledaniel235@gmail.com>
2025-05-07 07:41:36 +0100

    Update READMEs and fix frontend wallet connection

--- challenge-3-frontend/components/navbar.tsx
+++ challenge-3-frontend/components/navbar.tsx
@@ -1,32 +1,41 @@
 import Link from "next/link";
+import { ConnectButton } from '@rainbow-me/rainbowkit'; // Import ConnectButton
 
 export default function Navbar() {
   return (
-    <div className="flex flex-wrap items-center justify-center w-full gap-2">
-      <Link className="text-sm underline underline-offset-4" href="/">
-        Home
-      </Link>
-      <Link className="text-sm underline underline-offset-4" href="/wallet">
-        Wallet
-      </Link>
-      <Link
-        className="text-sm underline underline-offset-4"
-        href="/send-transaction"
-      >
-        Send transaction
-      </Link>
-      <Link
-        className="text-sm underline underline-offset-4"
-        href="/write-contract"
-      >
-        Write contract
-      </Link>
-      <Link
-        className="text-sm underline underline-offset-4"
-        href="/mint-redeem-lst-bifrost"
-      >
-        Mint/Redeem LST Bifrost
-      </Link>
-    </div>
+    <nav className="flex flex-wrap items-center justify-between w-full p-4 gap-2 border-b mb-4">
+      <div className="flex flex-wrap items-center justify-center gap-2">
+        <Link className="text-sm underline underline-offset-4" href="/">
+          Home
+        </Link>
+        <Link className="text-sm underline underline-offset-4" href="/token-vesting">
+          Token Vesting
+        </Link>
+        <Link className="text-sm underline underline-offset-4" href="/wallet">
+          Wallet
+        </Link>
+        <Link
+          className="text-sm underline underline-offset-4"
+          href="/send-transaction"
+        >
+          Send transaction
+        </Link>
+        <Link
+          className="text-sm underline underline-offset-4"
+          href="/write-contract"
+        >
+          Write contract
+        </Link>
+        <Link
+          className="text-sm underline underline-offset-4"
+          href="/mint-redeem-lst-bifrost"
+        >
+          Mint/Redeem LST Bifrost
+        </Link>
+      </div>
+      <div>
+        <ConnectButton /> {/* Add the ConnectButton here */}
+      </div>
+    </nav>
   );
 }
